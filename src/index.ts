a2f8c6515717be0a77a059deda8e8b964add9e48
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-08-31 17:46:48 +0300

    canister updated

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,40 @@
 import {
-    $query,
-    $update,
-    Record,
-    StableBTreeMap,
-    Vec,
-    nat64,
-    ic, 
-    Opt,
-    int32
+  $query,
+  $update,
+  Record,
+  StableBTreeMap,
+  Vec,
+  nat64,
+  ic,
+  Opt,
+  int32,
+  Principal
 } from "azle";
 
 type User = Record<{
-    id: int32;
-    username: string;
-    createdAt: nat64;
+  id: int32;
+  username: string;
+  owner: Principal; // Add owner field
+  createdAt: nat64;
 }>;
 
 type Doctor = Record<{
-    id: int32;
-    username: string;
-    createdAt: nat64;
+  id: int32;
+  username: string;
+  owner: Principal; // Add owner field
+  createdAt: nat64;
 }>;
 
 type Booking = Record<{
-    id: int32;
-    userId: int32;
-    doctorId: int32;
-    createdAt: nat64;
+  id: int32;
+  userId: int32;
+  doctorId: int32;
+  createdAt: nat64;
 }>;
 
 type BookingPayload = Record<{
-    userId: int32,
-    doctorId: int32
+  userId: int32;
+  doctorId: int32;
 }>;
 
 const users = new StableBTreeMap<int32, User>(0, 38, 1000);
@@ -39,245 +42,174 @@ const doctors = new StableBTreeMap<int32, Doctor>(1, 38, 2440);
 const bookings = new StableBTreeMap<int32, Booking>(2, 38, 4000);
 
 $update;
-export function createUser(username: string): User{
-    if(!username) {
-        throw new Error("No username provided!");
-    }
+export function createUser(username: string): User {
+  if (!username) {
+    throw new Error("No username provided!");
+  }
 
-    // Check for an existing user
-    const checkUser = users.values().find((user => user.username === username));
-    if(checkUser) {
-        throw new Error("Username already exist!");
-    }
+  // Check for an existing user
+  const existingUser = users.values().find((user) => user.username === username);
+  if (existingUser) {
+    throw new Error("Username already exists!");
+  }
 
-    // Add a new user to the users record
-    try {
-        const user: User = {
-            id: users.isEmpty() ? 1 : generateId("users"),
-            username,
-            createdAt: ic.time()
-        }
-        users.insert(user.id, user);
-        return user;
-    } catch(err) {
-        throw new Error(`Error creating a new user ${err}`);
-    }
+  // Add a new user to the users record
+  const caller = ic.provisional_create_canister({ controller: ic.caller() });
+  const newUser: User = {
+    id: generateId("users"),
+    username,
+    owner: caller,
+    createdAt: ic.time(),
+  };
+  users.insert(newUser.id, newUser);
+  return newUser;
 }
 
-$query
+$query;
 export function getUsers(): Vec<User> {
-    try {
-        return users.values();
-    } catch (err) {
-        throw new Error(`Failed to fetch users${err}`);
-    };
+  return users.values();
 }
 
 $query;
 export function getUser(id: int32): Opt<User> {
-    // Data input validation
-    if(!id) {
-        throw new Error("Invalid user id!");
-    }
-
-    try {
-        return users.get(id);
-    } catch (err) {
-        throw (`Couldn't find a user with the specified id ${err}`);
-    }
+  if (!id) {
+    throw new Error("Invalid user id!");
+  }
+  return users.get(id);
 }
 
 $update;
 export function deleteUser(id: int32): Opt<User> {
-    // Data input validation
-    if(!id) {
-        throw new Error("Invalid user id!");
-    }
+  if (!id) {
+    throw new Error("Invalid user id!");
+  }
 
-    // Delete users bookings
-    try {
-        bookings.values().forEach((booking => {
-            if (booking.userId === id) {
-                bookings.remove(booking.id);
-            }
-        }))
-    } catch(err) {
-        throw new Error(`Couldn't delete users bookings`);
+  // Delete users bookings
+  bookings.values().forEach((booking) => {
+    if (booking.userId === id) {
+      bookings.remove(booking.id);
     }
+  });
 
-    try {
-        return users.remove(id);
-    } catch (err) {
-        throw `Couln't delete user ${err}`;
-    }
+  return users.remove(id);
 }
 
 $update;
 export function createDoctor(username: string): Doctor {
-    // Input validation for username
-    if(!username) {
-        throw new Error(`Username field required!`);
-    }
+  if (!username) {
+    throw new Error("Username field required!");
+  }
 
-    // Check for an existing doctor
-    const checkDoctor = doctors.values().find((doc => doc.username === username));
-    if(checkDoctor) {
-        throw new Error("Username already exist!");
-    }
+  // Check for an existing doctor
+  const existingDoctor = doctors.values().find((doc) => doc.username === username);
+  if (existingDoctor) {
+    throw new Error("Username already exists!");
+  }
 
-    // Create new doctor record and insert into doctors tree.
-    try {
-        const doctor: Doctor = {
-            id: doctors.isEmpty() ? 1 : generateId("doctors"),
-            username,
-            createdAt: ic.time()
-        };
-        doctors.insert(doctor.id, doctor);
-        return doctor;
-    } catch(err) {
-        throw new Error(`Couldn't add a new doctor record ${err}`);
-    }
+  // Create new doctor record and insert into doctors tree.
+  const caller = ic.provisional_create_canister({ controller: ic.caller() });
+  const newDoctor: Doctor = {
+    id: generateId("doctors"),
+    username,
+    owner: caller,
+    createdAt: ic.time(),
+  };
+  doctors.insert(newDoctor.id, newDoctor);
+  return newDoctor;
 }
 
 $query;
 export function getDoctors(): Vec<Doctor> {
-    try {
-        return doctors.values();
-    } catch(err) {
-        throw new Error(`Error fetching doctors ${err}`);
-    }
+  return doctors.values();
 }
 
 $query;
 export function getDoctor(id: int32): Opt<Doctor> {
-    // validate input field
-    if(!id) {
-        throw new Error("Invalid id field")
-    }
-
-    try {
-        return doctors.get(id);
-    } catch(err) {
-        throw new Error(`Error fetching doctor ${err}`)
-    }
+  if (!id) {
+    throw new Error("Invalid id field");
+  }
+  return doctors.get(id);
 }
 
 $update;
 export function deleteDoctor(id: int32): Opt<Doctor> {
-    // Validate input field
-    if(!id) {
-        throw new Error("Invalid id input field");
-    }
+  if (!id) {
+    throw new Error("Invalid id input field");
+  }
 
-    // delete bookings made to the doctor
-    try {
-        bookings.values().forEach((booking => {
-            if (booking.doctorId === id) {
-                bookings.remove(booking.id);
-            }
-        }));
-    } catch(err) {
-        throw new Error(`Couldn't delete bookings made to the doctor ${err}`);
+  // Delete bookings made to the doctor
+  bookings.values().forEach((booking) => {
+    if (booking.doctorId === id) {
+      bookings.remove(booking.id);
     }
+  });
 
-    try {
-        return doctors.remove(id);
-    } catch(err) {
-        throw new Error(`Couldn't delete doctor ${err}`);
-    }
+  return doctors.remove(id);
 }
 
 $update;
 export function createBooking(payload: BookingPayload): Booking {
-    // validate input fields
-    const { userId, doctorId } = payload;
-    if(!userId || !doctorId) {
-        throw new Error(`Invalid input fields`);
-    }
+  const { userId, doctorId } = payload;
+  if (!userId || !doctorId) {
+    throw new Error("Invalid input fields");
+  }
 
-    // Validate user and doctor existing records
-    const user = users.containsKey(userId);
-    const doc = doctors.containsKey(doctorId);
-    if(!doc || !user) {
-        throw new Error("user or doctor with the given Id does not exist");
-    }
+  // Validate user and doctor existing records
+  const userExists = users.containsKey(userId);
+  const doctorExists = doctors.containsKey(doctorId);
+  if (!doctorExists || !userExists) {
+    throw new Error("User or doctor with the given Id does not exist");
+  }
 
-    // Check for existing booking
-    const checkBooking = bookings.values().find(
-        (
-            booking => booking.userId === userId && booking.doctorId === doctorId
-            ));
-    if (checkBooking) {
-        throw new Error("A session has already been booked!");
-    }
+  // Check for an existing booking
+  const existingBooking = bookings.values().find(
+    (booking) => booking.userId === userId && booking.doctorId === doctorId
+  );
+  if (existingBooking) {
+    throw new Error("A session has already been booked!");
+  }
 
-    // Create new booking record
-    try {
-        const booking: Booking = {
-            id: bookings.isEmpty() ? 1 : generateId("bookings"),
-            userId,
-            doctorId,
-            createdAt: ic.time()
-        }
-        bookings.insert(booking.id, booking);
-        return booking;
-    }
-    catch(err) {
-        throw new Error(`Couldn't book the session! ${err}`);
-    }
+  // Create new booking record
+  const newBooking: Booking = {
+    id: generateId("bookings"),
+    userId,
+    doctorId,
+    createdAt: ic.time(),
+  };
+  bookings.insert(newBooking.id, newBooking);
+  return newBooking;
 }
 
 $query;
 export function getUserBookings(id: int32): Vec<Booking> {
-    // Validate input field
-    if(!id) {
-        throw new Error(`Invalid id input field!`);
-    }
-
-    // Get the bookings made by the user 
-    try {
-        return bookings.values().filter((booking => booking.userId === id));
-    } catch(err) {
-        throw new Error(`Couldn't find users booking ${err}`);
-    }
+  if (!id) {
+    throw new Error("Invalid id input field!");
+  }
+  return bookings.values().filter((booking) => booking.userId === id);
 }
 
 $query;
 export function getDoctorBookings(id: int32): Vec<Booking> {
-    // Validate input field
-    if(!id) {
-        throw new Error(`Invalid id input field!`);
-    }
-
-    // Get doctor's bookings 
-    try {
-        return bookings.values().filter((booking => booking.doctorId === id));
-    } catch(err) {
-        throw new Error(`Couldn't find users booking ${err}`);
-    }
+  if (!id) {
+    throw new Error("Invalid id input field!");
+  }
+  return bookings.values().filter((booking) => booking.doctorId === id);
 }
 
 $update;
 export function deleteBooking(id: int32): Opt<Booking> {
-    // Validate id input field
-    if(!id) {
-        throw new Error('Invalid input field!');
-    }
-
-    try {
-        return bookings.remove(id);
-    } catch(err) {
-        throw new Error(`Couldn't delete booking! ${err}`);
-    }
+  if (!id) {
+    throw new Error("Invalid input field!");
+  }
+  return bookings.remove(id);
 }
 
 function generateId(recordName: string): int32 {
-    // Generate ids based on the last id of the record
-    if(recordName === "users") {
-        return users.values().slice(-1)[0].id + 1;
-    } else if(recordName === "doctors") {
-        return doctors.values().slice(-1)[0].id + 1;
-    } else {
-        return bookings.values().slice(-1)[0].id + 1;
-    }
+  if (recordName === "users") {
+    return users.isEmpty() ? 1 : users.values().slice(-1)[0].id + 1;
+  } else if (recordName === "doctors") {
+    return doctors.isEmpty() ? 1 : doctors.values().slice(-1)[0].id + 1;
+  } else {
+    return bookings.isEmpty() ? 1 : bookings.values().slice(-1)[0].id + 1;
+  }
 }
